7edd7727f259763b70a90086e328dfa2991ceb15
Shushant <profshushant@gmail.com>
2025-03-03 16:40:56 +0530

    first commit

---
+++ Testing4/src/main/java/code/services/CalculatorService.java
@@ -0,0 +1,25 @@
+package code.services;
+
+public class CalculatorService {
+
+    public static int sum(int a, int b){
+        return  a+b;
+    }
+
+    public static int product(int a, int b){
+        return  a*b;
+    }
+
+    public static double divide(int a, int b){
+        return  (double) a/b;
+    }
+
+    public static int sumOfNumbers(int... numbers){
+        int sum =0;
+        for(int n: numbers){
+            sum+=n;
+        }
+        return sum;
+    }
+
+}
---
+++ Testing4/src/test/java/code/service/CalculatorServiceTest.java
@@ -0,0 +1,45 @@
+package code.service;
+
+import code.services.CalculatorService;
+import org.junit.*;
+
+import java.util.Date;
+
+public class CalculatorServiceTest {
+
+    @BeforeClass
+    public static void testStart(){
+        System.out.println("Tests Starts: "+ new Date());
+    }
+
+    @Before
+    public void beforeAnyTest(){
+        System.out.println("Before any test");
+    }
+
+    @After
+    public void afterAnyTest(){
+        System.out.println("After any test");
+    }
+
+    @Test
+    public void sumTest(){
+        System.out.println("Test for sumTest");
+        int result = CalculatorService.sum(30, 23);
+        int expected = 53;
+        Assert.assertEquals(expected,result);
+    }
+
+    @Test
+    public void sumOfNumbers(){
+        System.out.println("Test for sumOfNumbers");
+        int result = CalculatorService.sumOfNumbers(23, 12, 43, 54);
+        int expected =132;
+        Assert.assertEquals(expected,result);
+    }
+
+    @AfterClass
+    public static void testEnd(){
+        System.out.println("Tests Ends: "+ new Date());
+    }
+}
